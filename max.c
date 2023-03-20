c7e9933fb8a6927e9694ca018d4b32ed2d2e1de7
yhdego <tesfahiwetzuabr@gmail.com>
2023-03-20 11:38:24 +0400

    third file

---
+++ max.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+int max(int num1, int num2){
+    int result;
+    if (num1 > num2)
+    {
+        result = num1;
+    }else{
+        result = num2;
+    }
+    return result;
+}
+
+int main(){
+    printf("%d", max(5,6));
+    return 0;
+}
