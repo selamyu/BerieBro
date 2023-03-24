347a6bbd4e5abb48485df4345214b1e6ed7d02a7
yhdego <tesfahiwetzuabr@gmail.com>
2023-03-24 10:05:37 +0400

    is that array satisfies

---
+++ Array235.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int is235Array(int a[ ], int len){
+    int c2=0, c3=0, c5=0, cc=0;
+    
+    for (int i = 0; i < len; i++)
+    {
+        if (a[i]%2==0){
+            c2++;
+        }
+        if (a[i]%3==0){
+            c3++;
+        }
+        if (a[i]%5==0){
+            c5++;
+        }
+        if (a[i]%2!=0 && a[i]%3!=0 && a[i]%5!=0){
+            cc++;
+        } 
+    }
+
+    if (c2+c3+c5+cc==len){
+        return 1;
+    }else{
+        return 0;
+    }
+}
+
+int main(){
+    int arr[] = {2, 3, 5, 7, 11};
+    printf("%d", is235Array(arr, 5));
+    return 0;
+}
