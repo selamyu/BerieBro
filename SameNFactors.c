0c743e2bdfdad916b20ed348717a405d031fe829
yhdego <tesfahiwetzuabr@gmail.com>
2023-03-27 14:23:25 +0400

    same number of factors

---
+++ SameNFactors.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int sameNumberOfFactors(int n1, int n2){
+    int count1=0, count2=0;
+    for (int i = 1; i < n1; i++)
+    {
+        if (n1%i==0)
+        {
+            count1++;
+        } 
+    }
+    for (int j = 1; j < n2; j++)
+    {
+        if (n2%j==0)
+        {
+            count2++;
+        } 
+    }
+
+    if (count1==count2)
+    {
+        return 1;
+    }else{
+        return 0;
+    }   
+}
+
+int main(){
+    int n1 = 8;
+    int n2 = 12;
+    printf("%d", sameNumberOfFactors(n1, n2));
+    return 0;
+}
