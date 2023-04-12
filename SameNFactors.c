1bb1e6195fa7305bb44b80649cd4c034d6c397ee
yhdego <tesfahiwetzuabr@gmail.com>
2023-04-12 10:57:33 +0400

    updated twin code

--- SameNFactors.c
+++ SameNFactors.c
@@ -2,14 +2,20 @@
 
 int sameNumberOfFactors(int n1, int n2){
     int count1=0, count2=0;
-    for (int i = 1; i < n1; i++)
+
+    if (n1<0 || n2 <0)
+    {
+        return -1;
+    }
+    
+    for (int i = 1; i <= n1; i++)
     {
         if (n1%i==0)
         {
             count1++;
         } 
     }
-    for (int j = 1; j < n2; j++)
+    for (int j = 1; j <= n2; j++)
     {
         if (n2%j==0)
         {
@@ -26,8 +32,8 @@ int sameNumberOfFactors(int n1, int n2){
 }
 
 int main(){
-    int n1 = 8;
-    int n2 = 12;
+    int n1 = 0;
+    int n2 = 0;
     printf("%d", sameNumberOfFactors(n1, n2));
     return 0;
 }
--- Twinprime.c
+++ Twinprime.c
@@ -1,28 +1,21 @@
 #include <stdio.h>
 
-int isPrime(int n){
+int isTwinPrime(int n){
     for (int i = 2; i <= n/2; i++)
     {
        if (n%i==0)   //&& ((n-2)%i != 0 || (n+2) % i != 0)
        {
         return 0;
        } 
-    }
-    return 1;  
-}
-
-int isTwinPrime(int n){
-    if (isPrime(n)&& (isPrime(n-2) || isPrime(n+2)))
-    {
+       else if (n%i !=0 && ((n-2)!=0 || (n+2)%i !=0))
+       {
         return 1;
-    }
-    else{
-        return 0;
-    }   
+       } 
+    } 
 }
 
 int main(){
-    int n = 11;
+    int n = 8;
     printf("%d", isTwinPrime(n));
     return 0;
 }
